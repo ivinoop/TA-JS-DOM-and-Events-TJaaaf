b4fe4ad7c7feabcb0284eb4b332ca59688853355
= <=>
2021-06-02 21:44:32 +0530

    Block 2 Final

--- block-BJaaer/code/script.js
+++ block-BJaaer/code/script.js
@@ -27,14 +27,21 @@ let close = document.querySelector('.modal-close');
     modal.style.display = "none";
   });
 
-let submit = document.querySelector('.submit');
-submit.addEventListener('click', () => {
-});
-
 function displayInfo(data) {
   console.log(data);
   let h1 = document.createElement('h1');
   h1.innerText = `Hello ${data.name}`;
-  modalInfo.append(h1);
+  let email = document.createElement('email');
+  email.innerText = `Email = ${data.email}`;
+  let love = document.createElement('love');
+  love.innerText = `TV choice = ${data.love}`;
+  let color = document.createElement('color');
+  color.innerText = `Fav Color = ${data.color}`;
+  let range = document.createElement('range');
+  range.innerText = `Inception Movie Rating = ${data.range}`;
+  let book = document.createElement('book');
+  book.innerText = `Fav Book Genre ${data.book}`;
+  
+  modalInfo.append(h1,email,love,color,range,book);
 } 
 
